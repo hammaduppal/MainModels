c02a7d37f89c1c86b60a1bce1ebb9a1a953d33b8
Hammad <uppal.hammad@gmail.com>
2025-07-21 23:52:20 +0500

    Further More Changes in My Task

--- MainModels/DTOModels/AppConstants.cs
+++ MainModels/DTOModels/AppConstants.cs
@@ -21,13 +21,29 @@ namespace MainModels.DTOModels
             [Description("Receiving")]
             Receiving = 3
         }
+        public enum RequisitionTypes
+        {
+            [Description("Requisition")]
+            Requisition = 1,
+
+            [Description("Purchase Order")]
+            PurchaseOrder = 2,
+
+        }
+        public enum PurchaseOrderTypes
+        {
+            [Description("Purchase Order")]
+            PurchaseOrder = 2,
+
+            [Description("Receiving")]
+            Receiving = 3
+        }
         public enum PurchaseStatus
         {
             Draft = 1,
-            Submitted = 2,
-            Approved = 3,
-            Rejected = 4,
-            Completed = 5
+            Approved = 2,
+            Rejected = 3,
+            Completed = 4
         }
       
 
