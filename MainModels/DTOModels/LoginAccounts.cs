7faae6055518dd961f33a0fdda09da200aea49ad
Hammad <ceo@inspirealert.com>
2025-07-26 01:31:18 +0500

    Enable Swagger

---
+++ MainModels/DTOModels/LoginAccounts.cs
@@ -0,0 +1,12 @@
+﻿namespace MainModels.DTOModels
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+    }
+    public class LoginAccounts
+    {
+    }
+}
